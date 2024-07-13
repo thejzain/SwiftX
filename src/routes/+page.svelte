ce6cbd8ed62e13b6f70c9fad0f3c37d7b791fc12
Thej Zain <thejzainkp@gmail.com>
2024-07-13 22:52:01 +0530

    0

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- A very basic Hero page, prompting the user to either signup or login -->
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Welcome to BasicPay!</h1>
+            <p class="py-6">The app that lets you pay, <em>basically</em>, anyone.</p>
+            <!-- These SvelteKit preload attributes keep the redirects from firing when the user hovers over the button -->
+            <div data-sveltekit-preload-data="tap" data-sveltekit-preload-code="hover">
+                <a href="/signup" class="btn-primary btn">Signup Now</a>
+                <a href="/login" class="btn-outline btn">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/routes/dashboard/components/Footer.svelte
@@ -0,0 +1,37 @@
+<!--
+@component
+
+The `Footer` component implements a very simple footer that displays links to
+SDF's terms of service, privacy policy, and the GitHub repo where this project
+can be found.
+-->
+
+<script>
+    // We import things from external packages that will be needed
+    import { GithubIcon } from 'svelte-feather-icons'
+</script>
+
+<footer class="footer items-center bg-base-300 p-4">
+    <div class="grid-flow-col items-center">
+        <ul class="menu menu-vertical md:menu-horizontal">
+            <li>
+                <a href="https://www.stellar.org/terms-of-service" target="_blank"
+                    >Terms of Service</a
+                >
+            </li>
+            <li>
+                <a href="https://www.stellar.org/privacy-policy" target="_blank">Privacy Policy</a>
+            </li>
+        </ul>
+    </div>
+    <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <ul class="menu">
+            <li>
+                <a href="https://github.com/stellar/basic-payment-app" target="_blank">
+                    <GithubIcon size="16" />
+                    GitHub
+                </a>
+            </li>
+        </ul>
+    </div>
+</footer>
